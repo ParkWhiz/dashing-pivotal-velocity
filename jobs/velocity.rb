e4736d66792280eab54e1d0531f9bc3c50a52546
Mark A. Stratman <stratman@gmail.com>
2015-07-14 16:16:14 -0500

    Fixed current velocity, disregard future iterations, and unaccepted stories

--- jobs/velocity.rb
+++ jobs/velocity.rb
@@ -4,24 +4,21 @@ require 'tracker_api'
 # to widgets with IDs of 'pivotal-<project_id>' (e.g. 'pivotal-1133342')
 projects = [999999,999998]
 
-@client = TrackerApi::Client.new(token: ENV['PIVOTAL_API_TOKEN'])
-
-def get_velocities(project_id)
-  project = @client.project(project_id, fields: ':default,current_volatility')
-
-  iterations = project.iterations(fields: 'start,stories(estimate)')
-  iterations.map.with_index(1) do |iteration, x|
-    {
-      #"start" => iteration.start.strftime("%b %d"),
-      "x"     => x,
-      "y"     => iteration.stories.reduce(0) { |points,story| points += (story.estimate || 0) }
-    }
-  end
-end
+client = TrackerApi::Client.new(token: ENV['PIVOTAL_API_TOKEN'], logger:Logger.new($stdout))
 
 SCHEDULER.every '10m', :first_in => 0 do
   projects.each do |project_id|
-    velocities = get_velocities(project_id)
-    send_event("pivotal-#{project_id}", points: velocities, displayedValue: velocities[-2]["y"])
+    project = client.project(project_id, fields: ':default,current_volatility,current_velocity')
+
+    iterations = project.iterations(fields: 'start,stories(estimate,accepted_at)').select{ |i| i.start <= DateTime.now }
+    velocities = iterations.map.with_index(1) do |iteration, x|
+      {
+        #"start" => iteration.start.strftime("%b %d"),
+        "x"     => x,
+        "y"     => iteration.stories.reject { |s| s.accepted_at.nil? }.reduce(0) { |points,story| points += (story.estimate || 0) }
+      }
+    end
+
+    send_event("pivotal-#{project_id}", points: velocities, displayedValue: project.current_velocity)
   end
 end
